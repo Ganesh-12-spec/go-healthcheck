151818b0f55633684df74de2c30b33e2eced9c58
Ganesh Mhetre <mhetreg458@gmail.com>
2026-07-24 06:52:49 +0000

    feat: config loader

---
+++ checker/config.go
@@ -0,0 +1,25 @@
+package checker
+
+import (
+	"encoding/json"
+	"os"
+)
+
+type Config struct{
+	URLs            []string `json:"urls"`
+	TimeoutSeconds  int    `json:"timeout-seconds"`
+	MaxConcurrent  int      `json:"max_concurrent"`
+}
+
+func LoadConfig(path string) (*Config, error){
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	var cfg Config
+	err = json.Unmarshal(data, &cfg)
+	if err != nil {
+		return nil, err
+	}
+	return &cfg, nil
+}
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/Ganesh-12-spec/go-healthcheck/checker"
+)
+
+func main() {
+	cfg, err := checker.LoadConfig("config.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Loaded config: %+v\n", cfg)
+}
