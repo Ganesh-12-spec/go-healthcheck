4c9aae7ecfe03701fae04272b069b16dfdaf9180
Ganesh Mhetre <mhetreg458@gmail.com>
2026-07-25 16:58:44 +0000

    feat: worker pool with semaphore to limit concurrency

--- checker/checker.go
+++ checker/checker.go
@@ -7,45 +7,49 @@ import (
 )
 
 type Result struct{
-	URl        string
-  StatusCode int
+	URL        string
+	StatusCode int
 	Duration   time.Duration
 	Err        error
 }
 
-func CheckURl(url string) Result {
+func CheckURL(url string) Result {
 	start := time.Now()
 	resp,err := http.Get(url)
 	duration := time.Since(start)
 	
 
 	if err != nil{
-		return  Result{URl :url,Err:err,Duration:duration}
+		return  Result{URL: url, Err:err, Duration:duration}
 	}
 	defer resp.Body.Close()
 
-	return Result{URl: url, StatusCode: resp.StatusCode, Duration:duration}
+	return Result{URL: url, StatusCode: resp.StatusCode, Duration:duration}
 }
-func CheckALL(urls []string) []Result {
+func CheckALL(urls []string, maxConcurrent int) []Result {
 	var wg sync.WaitGroup
 	resultsChan := make(chan Result, len(urls))
+	sem := make(chan struct{}, maxConcurrent)
 
-	for _,url := range urls {
+	for _, url := range urls {
 		wg.Add(1)
-		go func(u string){
+		go func(u string) {
 			defer wg.Done()
-			result := CheckURl(u)
+			// hint: sem <- struct{}{} — slot le
+			sem <- struct{}{}
+			// hint: defer func() { <-sem }() — slot free karo function khatam hote hi
+			defer func() {<-sem }()
+			result := CheckURL(u)
 			resultsChan <- result
 		}(url)
 	}
+
 	wg.Wait()
-  close(resultsChan)
+	close(resultsChan)
 
 	var results []Result
-	for r:= range resultsChan{
+	for r := range resultsChan {
 		results = append(results, r)
-
 	}
 	return results
-
 }
